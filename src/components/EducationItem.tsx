f69f89cf2d34646d8d5689c024efd0086b2f530a
Minh Vương <hardingadonis@gmail.com>
2024-04-03 17:56:51 +0700

    update EducationItem component

--- src/components/EducationItem.tsx
+++ src/components/EducationItem.tsx
@@ -5,11 +5,11 @@ type EducationItemProps = {
 const EducationItem = (props: EducationItemProps) => {
 	return (
 		<>
-			<div className="mb-4 flex justify-center">
+			<div className="mb-4 w-full flex justify-center">
 				<div className="w-1/4">
 					<p className="text-sm font-semibold">{props.education.duration}</p>
 				</div>
-				<div className="w-3/4 ml-4">
+				<div className="w-2/4">
 					<h3 className="text-xl font-bold">{props.education.college}</h3>
 					<p className="text-sm font-normal">{props.education.major}</p>
 					<p className="text-sm font-semibold">GPA: {props.education.gpa}</p>
